db2e7589922c2d4d52885bceaa81f40b5d856c75
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-05-29 19:12:33 +0200

    initial commit

---
+++ src/main/java/com/example/cwiczenie2/Cwiczenie2Application.java
@@ -0,0 +1,13 @@
+package com.example.cwiczenie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cwiczenie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cwiczenie2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cwiczenie2/Cwiczenie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cwiczenie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cwiczenie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
